77fd8c26e533ce22b35a421a88c0c4c995dd5d80
aungkokomin <aungkokomin.webdeveloper@gmail.com>
2025-12-22 18:07:54 +0800

    refactor: update commission-setting route list

--- backend/src/modules/commission-setting/commission-setting.routes.js
+++ backend/src/modules/commission-setting/commission-setting.routes.js
@@ -4,8 +4,8 @@ const commissionSettingController = require('./commission-setting.controller');
 
 router.get('/', authMiddleware, commissionSettingController.getAllCommissionSettings);
 router.get('/:id', authMiddleware, commissionSettingController.getCommissionSettingById);
-router.post('/', authMiddleware, commissionSettingController.createCommissionSetting);
-router.put('/:id', authMiddleware, commissionSettingController.updateCommissionSetting);
-router.delete('/:id', authMiddleware, commissionSettingController.deleteCommissionSetting);
+router.post('/create', authMiddleware, commissionSettingController.createCommissionSetting);
+router.put('/update/:id', authMiddleware, commissionSettingController.updateCommissionSetting);
+router.delete('/delete/:id', authMiddleware, commissionSettingController.deleteCommissionSetting);
 
 module.exports = router;
